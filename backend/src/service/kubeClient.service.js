38320734400a321b47e888d95605c803c11e5066
Gil Fitussi <gigo1985@gmail.com>
2026-05-23 19:02:24 +0300

    Restore reusable Kubernetes client factory helper

---
+++ backend/src/service/kubeClient.service.js
@@ -0,0 +1,34 @@
+const k8s = require("@kubernetes/client-node");
+const { getOcToken } = require("./ocAuth.service");
+
+async function createKubeConfig() {
+	const kubeConfig = new k8s.KubeConfig();
+	kubeConfig.loadFromDefault();
+
+	const token = await getOcToken();
+
+	if (!token) {
+		throw new Error("Unable to create Kubernetes client without an oc token");
+	}
+
+	const currentUser = kubeConfig.getCurrentUser();
+
+	if (!currentUser) {
+		throw new Error("No current Kubernetes user found in kubeconfig");
+	}
+
+	currentUser.token = token;
+
+	return kubeConfig;
+}
+
+async function createKubeClient(apiClientClass = k8s.CoreV1Api) {
+	const kubeConfig = await createKubeConfig();
+
+	return kubeConfig.makeApiClient(apiClientClass);
+}
+
+module.exports = {
+	createKubeClient,
+	createKubeConfig,
+};
